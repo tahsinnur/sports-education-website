de4330bb523fa01b4a47c3c18abaaf9b51a0bfdd
tahsinnur <tahsinnurtajin@gmail.com>
2021-10-04 23:22:10 +0600

    full n final

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import NotFound from './components/NotFound/NotFound';
